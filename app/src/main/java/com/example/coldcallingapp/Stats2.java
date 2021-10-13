e40c00ecb8b7fbf826baf3d86d10764007ae27b6
mingl1 <76068638+mingl1@users.noreply.github.com>
2021-10-13 09:28:32 -0400

    Update Stats2.java
    getting names of uncalled students

--- app/src/main/java/com/example/coldcallingapp/Stats2.java
+++ app/src/main/java/com/example/coldcallingapp/Stats2.java
@@ -3,4 +3,20 @@ package com.example.coldcallingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Stats2 extends AppCompatActivity{
+  
+private TextView name;
+
+protected void onCreate(Bundle savedInstanceState) {
+	name = findViewById(R.id.TextView);
+	MainActivity x = new MainActivity();
+	super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_cheat);
+  String list = "";
+  for (int x = 0; x < x.getDrawables().length; x++) {
+  	if (x.getCalledorNot()[x] == false {
+    	list += x.getDrawables[x];
+    }
+  }
+  name.setText(list);
+}    
 }
